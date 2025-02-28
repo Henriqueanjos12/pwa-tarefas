e2e9755b35ec1958a9ac6c602479857eec1b9fd1
L. Henrique <henriqueanjos12@gmail.com>
2025-02-28 13:13:51 -0300

    Atualização GitHub Pages

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ const ASSETS = [
     './js/db.js',
     './js/app.js',
     './manifest.json',
-    './icons/icon-192x192.png',
-    './icons/icon-512x512.png'
+    './images/icons/icon-192x192.png',
+    './images/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
